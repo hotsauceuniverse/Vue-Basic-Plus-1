1aeb0e837c128f99dea03eb78e9fadadcf112643
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-10 16:51:27 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,80 @@
+<template>
+  <h2 @[event]='add'>{{ msg }}</h2>
+  <h2 v-bind:[attr]='message'>{{ message }}</h2>
+  <Fruits/>
+  <h2>{{ reversedMessage }}</h2>
+  <h2 :class="{active: isActive}" @click="activate">Hello???({{ isActive }})</h2>
+  <h2 :style="fontStyle" @click="changeStyle">Hi</h2>
+  <h2 v-if="isShow">click button</h2>
+  <h2 v-else>Good~!!</h2>
+  <button @click="handler">click me!</button>
+  <h1>My Favorit Fruits</h1>
+  <ul>
+    <li v-for="(A, index) in B" :key="A">{{ A }}-{{ index + 1 }}</li>
+  </ul>
+</template>
+
+<script>
+import Fruits from './components/Fruits.vue'
+
+export default {
+  components: { Fruits },
+  
+  data() {
+    return {
+      B: ['Watermelon', 'Melon', 'Orange'],
+      isShow: true,
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px',
+      },
+      isActive: false,
+      alert: 'hello computed!',
+      msg: 'hello world!',  
+      message: 'action',
+      attr: 'class',
+      event: 'click'
+    }
+  },
+
+  component: {
+    Fruits
+  },
+
+  computed: {
+    reversedMessage() {
+      return this.alert.split('').reverse().join('')
+    },
+  },
+
+  methods: {
+    add() {
+    this.msg += '!'
+    },
+
+    activate() {
+    this.isActive = true
+    },
+
+    changeStyle() {
+      this.fontStyle.color = 'blue'
+      this.fontStyle.fontSize = '50px'
+    },
+
+    handler() {
+      this.isShow = !this.isShow
+    },
+  },
+}
+</script>
+
+<style scoped>
+  .action {
+    color: royalblue;
+  }
+
+  .active{
+    color : red;
+    font-weight: bold;
+  }
+</style>
---
+++ Fruits.vue
@@ -0,0 +1,38 @@
+<template>
+  <section v-if="hasFruits">
+    <h2>Fruits</h2>
+    <ul>
+      <li v-for="fruits in fruits" :key="fruits">{{ fruits }}</li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Reverse Fruits</h2>
+    <ul>
+      <li v-for="fruit in reverseFruits" :key="fruit">{{ fruit }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fruits: ['Banana', 'Apple', 'Cherry']
+    }
+  },
+
+  computed: {
+    hasFruits() {
+      return this.fruits.length > 0
+    },
+
+    reverseFruits() {
+      return this.fruits.map(fruit => {
+        //'Apple' => ['A', 'p', 'p', 'l', 'e'] => ['e', 'l', 'p', 'p', 'A'] => 'elppA'
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
